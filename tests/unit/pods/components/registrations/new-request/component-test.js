486364d4f8a4d8ea470e9f6427f62f2260e3b233
jjenkins <jjenkins@smithcarson.com>
2015-07-29 08:21:01 -0400

    fix broken tests replace x-toggle with checkbox so pages would load

--- tests/unit/pods/components/registrations/new-request/component-test.js
+++ tests/unit/pods/components/registrations/new-request/component-test.js
@@ -1,19 +1,20 @@
 import { moduleForComponent, test } from 'ember-qunit';
 
 moduleForComponent('registrations/new-request', 'Unit | Component | registrations/new request', {
-  // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar'],
-  unit: true
+    // Specify the other units that are required for this test
+    // needs: ['component:foo', 'helper:bar'],
+    needs: ['component:x-select', 'helper:fa-icon'],
+    unit: true
 });
 
-test('it renders', function(assert) {
-  assert.expect(2);
+test('it renders', function (assert) {
+    assert.expect(2);
 
-  // Creates the component instance
-  var component = this.subject();
-  assert.equal(component._state, 'preRender');
+    // Creates the component instance
+    var component = this.subject();
+    assert.equal(component._state, 'preRender');
 
-  // Renders the component to the page
-  this.render();
-  assert.equal(component._state, 'inDOM');
+    // Renders the component to the page
+    this.render();
+    assert.equal(component._state, 'inDOM');
 });
